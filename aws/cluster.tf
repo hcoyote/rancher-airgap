c99bc2a88d1e931f9fdc69e275f4ee105c21f7bb
Travis Campbell <hcoyote@ghostar.org>
2023-02-10 17:47:01 -0700

    flip to using aws_security_group_rule to deal with cyclic dependency

--- aws/cluster.tf
+++ aws/cluster.tf
@@ -117,197 +117,209 @@ resource "aws_volume_attachment" "volume_attachment" {
   instance_id = "${element(aws_instance.leader.*.id, count.index)}"
 }
 
+
+#
+# Security Group: Proxy controls
+#
+#
+#
 resource "aws_security_group" "proxy_sec_group" {
   name        = "${local.deployment_id}-proxy-sec-group"
   tags        = local.instance_tags
   description = "rancher registry proxy access"
-
-  # SSH access from anywhere
-  ingress {
-    description = "Allow anywhere inbound to ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # from anything to anything within the security group
-  ingress {
-    description = "from anything to anything within the security group"
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
-
-  # from anything to anything within the security group
-  egress {
-    description = "from anything to anything within the security group"
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
-  # from anything to anything within the security group
-  egress {
-    description = "from anything to anything within the security group"
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-
-  }
-
-  # outbound internet access
-  egress {
-    description = "disallow any outbound connections"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "allow access from node_sec_group hosts"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    security_groups = ["${aws_security_group.node_sec_group.id}"]
-  }
-  egress {
-    description = "allow access to node_sec_group hosts"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    security_groups = ["${aws_security_group.node_sec_group.id}"]
-  }
-
+  vpc_id      = aws_vpc.rancher_vpc.id
 
 }
 
+resource "aws_security_group_rule" "proxy_sec_group_ssh_inbound" {
+  type              = "ingress"
+  description       = "PROXY: allow ssh to any in security group"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.proxy_sec_group.id
+}
+
+resource "aws_security_group_rule" "proxy_sec_group_intra_inbound" {
+  type              = "ingress"
+  description       = "PROXY: allow to anything from anything within the security group"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  self              = true
+  security_group_id = aws_security_group.proxy_sec_group.id
+}
+
+resource "aws_security_group_rule" "proxy_sec_group_intra_outbound" {
+  type              = "egress"
+  description       = "PROXY: allow from anything to anything within the security group"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  self              = true
+  security_group_id = aws_security_group.proxy_sec_group.id
+}
+
+resource "aws_security_group_rule" "proxy_sec_group_any_outbound" {
+  type              = "egress"
+  description       = "PROXY: allow any outbound"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.proxy_sec_group.id
+}
+
+resource "aws_security_group_rule" "proxy_sec_group_from_node_sec_group" {
+  type              = "ingress"
+  description       = "PROXY: allow rancher sg to talk to proxy sg"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "tcp"
+  security_group_id = aws_security_group.proxy_sec_group.id
+  source_security_group_id = aws_security_group.node_sec_group.id
+}
+
+resource "aws_security_group_rule" "proxy_sec_group_to_node_sec_group" {
+  type              = "egress"
+  description       = "PROXY: allow proxy sg to talk to rancher sg"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "tcp"
+  security_group_id = aws_security_group.proxy_sec_group.id
+  source_security_group_id = aws_security_group.node_sec_group.id
+}
+
+
+#
+# Security Group: Rancher controls
+#
+#
+#
 resource "aws_security_group" "node_sec_group" {
   name        = "${local.deployment_id}-node-sec-group"
   tags        = local.instance_tags
   description = "rancher  ports"
+  vpc_id      = aws_vpc.rancher_vpc.id
 
-  # SSH access from anywhere
-  ingress {
-    description = "Allow anywhere inbound to ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+}
 
-  # HTTP access from anywhere to port 6443
-  ingress {
-    description = "rancher api"
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "node_sec_group_ssh_inbound" {
+  type              = "ingress"
+  description       = "RANCHER: allow ssh to any in security group"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.node_sec_group.id
+}
 
-  
-  # HTTP access from anywhere to port 6443
-  ingress {
-    description = "rancher manager ui"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "node_sec_group_intra_inbound" {
+  type              = "ingress"
+  description       = "RANCHER: allow to anything from anything within the security group"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  self              = true
+  security_group_id = aws_security_group.node_sec_group.id
+}
 
-  # from anything to anything within the security group
-  ingress {
-    description = "from anything to anything within the security group"
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
+resource "aws_security_group_rule" "node_sec_group_intra_outbound" {
+  type              = "egress"
+  description       = "RANCHER: allow from anything to anything within the security group"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  self              = true
+  security_group_id = aws_security_group.node_sec_group.id
+}
 
-  # outbound internet access
-  egress {
-    description = "disallow any outbound connections"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = []
-  }
+resource "aws_security_group_rule" "node_sec_group_from_proxy_sec_group" {
+  type              = "ingress"
+  description       = "RANCHER: allow proxy sg to talk to rancher sg"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "tcp"
+  security_group_id = aws_security_group.node_sec_group.id
+  source_security_group_id = aws_security_group.proxy_sec_group.id
+}
 
-  # from anything to anything within the security group
-  egress {
-    description = "from anything to anything within the security group"
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
+resource "aws_security_group_rule" "node_sec_group_to_proxy_sec_group" {
+  type              = "egress"
+  description       = "PROXY: allow proxy sg to talk to rancher sg"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "tcp"
+  security_group_id = aws_security_group.node_sec_group.id
+  source_security_group_id = aws_security_group.proxy_sec_group.id
+}
 
-  egress {
-    description = "allow outbound to releases rancher.com"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [
-      "13.33.252.31/32",
-      "13.33.252.27/32",
-      "13.33.252.49/32",
-      "13.33.252.45/32",
+
+resource "aws_security_group_rule" "node_sec_group_rancherapi_inbound" {
+  type              = "ingress"
+  description       = "RANCHER: allow any to rancher api in security group"
+  from_port         = 6443
+  to_port           = 6443 
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.node_sec_group.id
+}
+
+
+resource "aws_security_group_rule" "node_sec_group_rancherui_inbound" {
+  type              = "ingress"
+  description       = "RANCHER: allow any to rancher management in security group"
+  from_port         = 443
+  to_port           = 443 
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.node_sec_group.id
+}
+
+resource "aws_security_group_rule" "node_sec_group_rancherweb_outbound" {
+  type              = "egress"
+  description       = "RANCHER: allow any to rancher management in security group"
+  from_port         = 443
+  to_port           = 443 
+  protocol          = "tcp"
+  cidr_blocks = [
+      "13.33.252.31/32", # rancher.com
+      "13.33.252.27/32", # rancher.com
+      "13.33.252.49/32", # rancher.com
+      "13.33.252.45/32", # rancher.com
+      "152.195.19.97/32", # get.helm.sh
+      "104.21.42.101/32", # rke2.io
+      "172.67.204.246/32" # rke2.io
     ]
-  }
-  egress {
-    description = "allow outbound to get.helm.sh"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["152.195.19.97/32"]
-  }
-
-  egress {
-    description = "allow outbound to ubuntu mirror repos"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = var.ubuntu_cidrs
-  }
-  egress {
-    description = "allow outbound to ubuntu mirror repos"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.ubuntu_cidrs
-  }
-
-
-
-  egress {
-    description = "allow outbound to docker registry"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["44.196.175.70/32",
-      "52.3.144.121/32",
-      "54.165.156.197/32",
-      # registry-1.docker.io
-      "52.1.184.176/32",
-      "34.194.164.123/32",
-      "18.215.138.58/32",
-      # production.cloudflare.docker.com
-      "104.18.124.25/32",
-      "104.18.123.25/32",
-      "104.18.122.25/32",
-      "104.18.121.25/32",
-      "104.18.125.25/32",
-    ]
-  }
-
-  egress {
-    description = "allow explicit https to github.com"
-    from_port   = 443 
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [    "192.30.252.0/22",
+  security_group_id = aws_security_group.node_sec_group.id
+}
+resource "aws_security_group_rule" "node_sec_group_ubuntuhttps_outbound" {
+  type              = "egress"
+  description       = "RANCHER: allow outbound to ubuntu mirrors"
+  from_port         = 443
+  to_port           = 443 
+  protocol          = "tcp"
+  cidr_blocks       = var.ubuntu_cidrs
+  security_group_id = aws_security_group.node_sec_group.id
+}
+resource "aws_security_group_rule" "node_sec_group_ubuntuhttp_outbound" {
+  type              = "egress"
+  description       = "RANCHER: allow outbound to ubuntu mirrors"
+  from_port         = 80
+  to_port           = 80 
+  protocol          = "tcp"
+  cidr_blocks       = var.ubuntu_cidrs
+  security_group_id = aws_security_group.node_sec_group.id
+}
+resource "aws_security_group_rule" "node_sec_group_github_outbound" {
+  type              = "egress"
+  description       = "RANCHER: allow outbound to ubuntu mirrors"
+  from_port         = 80
+  to_port           = 80 
+  protocol          = "tcp"
+  cidr_blocks = [    
+     "192.30.252.0/22",
     "185.199.108.0/22",
     "140.82.112.0/20",
     "143.55.64.0/20",
@@ -320,16 +332,31 @@ resource "aws_security_group" "node_sec_group" {
     "20.200.245.247/32",
     "20.233.54.53/32"]
 
-  }
-
-  egress {
-    description = "allow explicit https to rke2.io for installs"
-    from_port   = 443 
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["104.21.42.101/32","172.67.204.246/32"]
-  }
+  security_group_id = aws_security_group.node_sec_group.id
 }
+
+
+
+  #egress {
+  #  description = "allow outbound to docker registry"
+  #  from_port   = 443
+  #  to_port     = 443
+  #  protocol    = "tcp"
+  #  cidr_blocks = ["44.196.175.70/32",
+  #    "52.3.144.121/32",
+  #    "54.165.156.197/32",
+  #    # registry-1.docker.io
+  #    "52.1.184.176/32",
+  #    "34.194.164.123/32",
+  #    "18.215.138.58/32",
+  #    # production.cloudflare.docker.com
+  #    "104.18.124.25/32",
+  #    "104.18.123.25/32",
+  #    "104.18.122.25/32",
+  #    "104.18.121.25/32",
+  #    "104.18.125.25/32",
+  #  ]
+  #}
 
 resource "aws_key_pair" "ssh" {
   key_name   = "${local.deployment_id}-key"
